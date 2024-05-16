f81544cda3b1702ae6d3d608099de4a4766851d1
Brennan <blfische92@gmail.com>
2024-05-16 17:14:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Challenge2._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string storedUserName = "Brennan";
+            string storedPassword = "password";
+            bool loginSuccess = false;
+
+            for (int i = 0; i < 3; i++) 
+            {
+                Console.WriteLine("You have " + (3 - i) + " login attempts remaining");
+                Console.WriteLine("Enter username");
+                string userName = Console.ReadLine();
+                Console.WriteLine("enter password");
+                string password = Console.ReadLine();
+                if (userName == storedUserName && password == storedPassword)
+                {
+                    loginSuccess = true;
+                    break;
+                }
+                Console.WriteLine("login attempt failed");
+
+            }
+            if (loginSuccess)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Account locked");
+            }
+            ;
+        }
+    }
+}
